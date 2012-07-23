48433e0db415e7779dcf4689f6200385fea37538
Bryn Austin Bellomy <bryn@signals.io>
2012-07-23 09:25:24 -0400

    bleh awake too long must commit before entropy occurs

--- iOS-BlingLord/SEBlingLordView.h
+++ iOS-BlingLord/SEBlingLordView.h
@@ -3,11 +3,12 @@
 //  SEBlingLord iOS springboard view
 //
 //  Created by bryn austin bellomy on 5/19/12.
-//  Copyright (c) 2012 bryn austin bellomy. All rights reserved.
+//  Copyright (c) 2012 robot bubble bath LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "SEBlingLordMenuItem.h"
+//#import "KJGridLayoutView.h"
 
 typedef enum {
   SEBlingLordLayoutStyleNone = 0,
@@ -17,8 +18,13 @@ typedef enum {
 
 static NSString *const SEBlingLordNotificationCloseView = @"SEBlingLordNotificationCloseView";
 
+//static inline KJGridPair SEGetGridPosition(NSUInteger newItemIdx, NSUInteger numCols) {
+//  NSUInteger col = newItemIdx % numCols;
+//  return KJGridPairMake((newItemIdx - col) / numCols, col);
+//}
 
-@interface SEBlingLordView : UIView <SEBlingLordMenuItemDelegate, UIScrollViewDelegate>
+
+@interface SEBlingLordView : UIView /*KJGridLayoutView*/ <SEBlingLordMenuItemDelegate, UIScrollViewDelegate>
 
 @property (nonatomic, assign, readwrite) BOOL allowsEditing;
 @property (nonatomic, assign, readwrite) BOOL isInEditingMode;
@@ -32,10 +38,11 @@ static NSString *const SEBlingLordNotificationCloseView = @"SEBlingLordNotificat
 @property (nonatomic, assign, readwrite) CGSize itemSize;
 @property (nonatomic, assign, readwrite) CGSize itemMargins;
 @property (nonatomic, assign, readwrite) CGSize outerMargins;
+//@property (nonatomic, assign, readwrite) KJGridPair gridSize;
 
 
 - (id) initWithFrame: (CGRect)frame;
-- (id) initWithFrame:(CGRect)frame itemSize:(CGSize)itemSize itemMargins:(CGSize)itemMargins outerMargins:(CGSize)outerMargins items:(NSMutableArray *)items;
+- (id) initWithFrame: (CGRect)frame itemSize:(CGSize)itemSize itemMargins:(CGSize)itemMargins outerMargins:(CGSize)outerMargins /*gridSize: (KJGridSize)gridSize*/ items:(NSMutableArray *)items;
 
 - (void) addMenuItem:(SEBlingLordMenuItem *)menuItem;
 - (void) addMenuItems:(NSArray *)menuItems;
