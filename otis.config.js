28a7507576bf2f06a197861dbad06ddf44748562
bryn austin bellomy <bryn.bellomy@gmail.com>
2012-09-27 19:50:11 -0400

    adding otis documentation generator config file

---
+++ otis.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  inDir: "./iOS-BlingLord",
+  outDir: "../gh-pages",
+  markdownEngine: "showdown",
+  onlyUpdated: false,
+  colourScheme: "friendly",
+  ignoreHidden: true,
+  sidebarState: true,
+  index: "SEBlingLordView.h.html",
+  exclude: "*.png,*.plist,*.pch"
+};
